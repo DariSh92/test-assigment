ca0f95f1a2560c1b220e78c497c5299dd0d23fa1
Darya Shlyakhovaya <dshlyakhovaya1992@gmail.com>
2023-07-06 17:44:19 +0300

    add rest task

---
+++ index.js
@@ -0,0 +1,23 @@
+function changeImage() {
+  const image = document.getElementById("Oddity");
+  image.src = "./images/oddity.jpeg";
+  image.alt = "Oddity";
+}
+
+// const googleImage = "./images/google_logo.svg";
+// const oddityImage = "./images/oddity.jpeg";
+// const isAktive = false;
+
+// function changeImage() {
+//   const image = document.getElementById("Oddity");
+
+//   if (isAktive) {
+//     image.src = googleImage;
+//     image.alt = "";
+//     isAktive = false;
+//   } else {
+//     image.src = oddityImage;
+//     image.alt = "";
+//     isAlternate = true;
+//   }
+// }
